e3adf981f654cc6032b8aa15d594cb3a42b5a95b
Эдуард Кусков <shadowfish@yandex.ru>
2024-07-10 05:58:33 +0300

    +user

---
+++ users/models.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+        # email = self.normalize_email(email)
+        # GlobalUserModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        # email = GlobalUserModel.normalize_username(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    objects = UserManager()
+
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=35, **NULLABLE, verbose_name="Телефон", help_text="Укажите телефон"
+    )
+    city = models.CharField(
+        max_length=150,
+        **NULLABLE,
+        verbose_name="Город",
+        help_text="Укажите город",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        **NULLABLE,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
