193338c589e4ea1ea9e8b50a47c1ae36d9583282
TheShadowfish <shadowfish@yandex.ru>
2024-07-25 12:48:56 +0300

    25.1 #1 #2 #3 done + star HOW TO TEST DATETIME?

--- courses/tests.py
+++ courses/tests.py
@@ -18,7 +18,7 @@ self.client.force_authenticate()
 """
 
 
-class LessonsTestCase(APITestCase):
+class CoursesTestCase(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create(email="admin@sky.pro")
@@ -73,7 +73,14 @@ class LessonsTestCase(APITestCase):
         response = self.client.get(url)
 
         data = response.json()
-        print(data)
+        # print(data)
+
+        created_text = str(self.course.created_at)
+        created = created_text[0:10]+'T'+created_text[11:26]+'Z'
+        updated_text = str(self.course.updated_at)
+        updated = updated_text[0:10] + 'T' + updated_text[11:26] + 'Z'
+        # print(f"created {created}")
+        # print(f"updated {updated}")
 
         result = {
             'count': 1,
@@ -100,91 +107,99 @@ class LessonsTestCase(APITestCase):
                                     'owner':  self.user.pk,
                                 }
                             ],
-                        'created_at': str(self.course.created_at),
-                        'updated_at': str(self.course.updated_at),
+                        'created_at': created,
+                        'updated_at': updated,
                         'subscriptions': False
                     }
                 ]
         }
-        #2024-07-25T07:54:27.529849Z
-        #Создает объект datetime из строки
-        # "2019-08-26T10:50:58.294041"
-        # datetime_obj = datetime.strptime(datetime_string, '%Y-%m-%dT%H:%M:%S.%f')
-
-
-        # 'created_at': '2024-07-25T07:43:42.219321Z', 'updated_at': '2024-07-25T07:43:42.219342Z'
-        print(f"created_at:{self.course.created_at} updated_at:{self.course.updated_at}")
+        # print(f"created_at:{self.course.created_at} updated_at:{self.course.updated_at}")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(data, result)
-#
-#
-# class BreedTestCase(APITestCase):
-#
-#     def setUp(self):
-#         self.user = User.objects.create(email="admin@sky.pro")
-#         self.breed = Breed.objects.create(name='Лабрадор', breed="Большая мохнатая красивая белая собака", owner=self.user)
-#         self.dog = Dog.objects.create(name="Гром", breed=self.breed, owner=self.user)
-#         self.client.force_authenticate(user=self.user)
-#
-#     def test_breed_retrieve(self):
-#         url = reverse("dogs:breeds-retrieve", args=(self.breed.pk,))
-#         response = self.client.get(url)
-#         data = response.json()
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(data.get('name'), self.breed.name)
-#
-#     def test_breed_create(self):
-#         url = reverse("dogs:breeds-create")
-#         data = {
-#             "name": "Овчарка"
-#                 }
-#         response = self.client.post(url, data)
-#
-#         data = response.json()
-#
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Dog.objects.all().count(), 1)
-#
-#     def test_breed_update(self):
-#         url = reverse("dogs:breeds-update", args=(self.breed.pk,))
-#         data = {"name": "Лайка"}
-#         response = self.client.patch(url, data)
-#
-#         data = response.json()
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(data.get('name'), "Лайка")
-#
-#     def test_breed_delete(self):
-#         url = reverse("dogs:breeds-delete", args=(self.breed.pk,))
-#         response = self.client.delete(url)
-#
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertEqual(Breed.objects.all().count(), 0)
-#
-#     def test_breed_list(self):
-#         url = reverse("dogs:breeds-list")
-#         response = self.client.get(url)
-#
-#         data = response.json()
-#         # print(data)
-#
-#         result = {
-#             'count': 1,
-#             'next': None,
-#             'previous': None,
-#             'results':
-#                 [
-#                     {'id': self.breed.pk,
-#                      'dogs': [self.dog.name],
-#                      'name': self.breed.name,
-#                      'breed': self.breed.breed,
-#                      'owner': self.user.pk
-#                      }
-#                 ]}
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(data, result)
-#
+
+class LessonsTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(email="admin@sky.pro")
+
+        self.course = Course.objects.create(title='Курс номер 1', description='Описание курса номер 1',  owner=self.user)
+
+        video = "https://www.youtube.com/watch?v=uC0jJGfDxtM&list=PLlb7e2G7aSpTFea2FYxp7mFfbZW-xavhL&index=1"
+
+        self.lesson = Lesson.objects.create(title='Урок номер 1', course=self.course , description="описание" , video_url=video, owner=self.user)
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_lesson_retrieve(self):
+        url = reverse("courses:lessons-retrieve", args=(self.lesson.pk,))
+        response = self.client.get(url)
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get('title'), self.lesson.title)
+
+    def test_lesson_create(self):
+        url = reverse("courses:lessons-create")
+        course_id = self.course.pk
+        print(self.course.pk)
+
+        data = {
+            "title": "lesson 2",
+            "course": course_id
+                }
+
+
+        response = self.client.post(url, data)
+
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Lesson.objects.all().count(), 2)
+
+    def test_lesson_update(self):
+        url = reverse("courses:lessons-update", args=(self.lesson.pk,))
+        data = {"title": "Updated lesson"}
+        response = self.client.patch(url, data)
+
+        data = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data.get('title'), "Updated lesson")
+
+    def test_lesson_delete(self):
+        url = reverse("courses:lessons-delete", args=(self.lesson.pk,))
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Lesson.objects.all().count(), 0)
+
+    def test_lesson_list(self):
+        url = reverse("courses:lessons-list")
+        response = self.client.get(url)
+
+        data = response.json()
+        # print(f"data {data}")
+
+
+        result = {'count': 1,
+             'next': None,
+             'previous': None,
+             'results':
+                 [
+                     {'id': self.lesson.pk,
+                      'title': self.lesson.title,
+                      'description': self.lesson.description,
+                      'preview': None,
+                      'video_url': self.lesson.video_url,
+                      'course': self.lesson.course.pk,
+                      'owner':  self.user.pk,
+                      }
+                 ]
+             }
+
+        # print(f"result {result}")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data, result)
+
